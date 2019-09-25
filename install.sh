3d9d4ff95e437133733bc19ea86c44d1b6f9ca1f
Shriharsha M <shriharsha05@gmail.com>
2019-09-25 12:43:17 +0530

    Updated Python Version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python3.6
+sudo apt-get install python3.7
 sudo apt install python3-pip
 sudo pip3 install virtualenv
 sudo apt-get install whois
